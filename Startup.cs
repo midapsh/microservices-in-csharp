971f20301a3774c602c261ccfd67a7075c304193
hspadim <henrique@spadim.com.br>
2021-10-05 20:16:44 -0300

    Data Layer - Repository

--- Startup.cs
+++ Startup.cs
@@ -31,6 +31,7 @@ namespace PlatformService
             services.AddDbContext<AppDbContext>(opt =>
                 opt.UseInMemoryDatabase("InMem")
             );
+            services.AddScoped<IPlatformRepo, PlatformRepo>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
